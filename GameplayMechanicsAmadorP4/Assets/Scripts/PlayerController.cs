0bdeaf6a9bc65b1b7d89110be55d13bc935fb7bf
TheFALCON736 <710228@student.dusd.net>
2023-08-31 11:39:02 -0700

    Started game
    started game.

---
+++ GameplayMechanicsAmadorP4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalpoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fowardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalpoint.transform.forward * fowardInput * speed);
+    }
+}
