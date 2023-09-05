24deea5891ebd4292155fed2f7f2b1bb3240c063
TheFALCON736 <710228@student.dusd.net>
2023-09-05 11:37:39 -0700

    Now on 4.4
    Now on 4.4

--- GameplayMechanicsAmadorP4/Assets/Scripts/PlayerController.cs
+++ GameplayMechanicsAmadorP4/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    private float PowerUpStrength = 15.0f; 
     private GameObject focalpoint;
+    public bool hasPowerUp = false;
+    public GameObject powerUpIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +24,37 @@ public class PlayerController : MonoBehaviour
         float fowardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalpoint.transform.forward * fowardInput * speed);
+
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            powerUpIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerUpCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * PowerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerUp set to " + hasPowerUp );
+        }
     }
 }
