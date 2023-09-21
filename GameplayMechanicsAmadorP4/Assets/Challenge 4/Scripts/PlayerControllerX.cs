56df00d7cf347fb283a37e5218ebb94a342ed7e4
TheFALCON736 <710228@student.dusd.net>
2023-09-21 11:25:36 -0700

    hi
    hi

--- GameplayMechanicsAmadorP4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ GameplayMechanicsAmadorP4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -14,6 +14,8 @@ public class PlayerControllerX : MonoBehaviour
 
     private float normalStrength = 10; // how hard to hit enemy without powerup
     private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    private float boost = 20; 
     
     void Start()
     {
@@ -29,6 +31,13 @@ public class PlayerControllerX : MonoBehaviour
 
         // Set powerup indicator position to beneath player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(focalPoint.transform.forward * boost, ForceMode.Impulse);
+            SmokeParticle.Play(); 
+          -
+        }
 
     }
 
